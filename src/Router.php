46bf4f1fcd779e131f76db04acdce9d059685377
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-16 17:54:52 +0200

    update router

--- src/Router.php
+++ src/Router.php
@@ -11,21 +11,26 @@ class Router
     {
         $this->_url = $url;
     }
+
     public function get($path, $callable)
     {
     }
 
-    // public function drive_check()
-    // {
-    //     echo '<pre>';
-    //     echo print_r($this->_routes);
-    //     echo '</pre>';
-    // }
+    public function post($path, $callable)
+    {
+    }
 
     public function drive_check()
     {
-        if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
-            throw new \Exception('Aucune route ne correspond');
-        }
+        echo '<pre>';
+        echo print_r($this->_routes);
+        echo '</pre>';
     }
+
+    // public function drive_check()
+    // {
+    //     if (!isset($this->routes[$_SERVER['REQUEST_METHOD']])) {
+    //         throw new \Exception('Aucune route ne correspond');
+    //     }
+    // }
 }
