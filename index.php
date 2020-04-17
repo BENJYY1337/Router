20b6fab90ccab0e12afd50f658d3b3e78cdf5fb0
BENJYY1337 <pernot.benjamin@outlook.fr>
2020-04-17 09:08:36 +0200

    push complet router

--- index.php
+++ index.php
@@ -2,15 +2,26 @@
 
 require 'vendor/autoload.php';
 
-$router = new App\Router($_GET['url']);
+//die($_GET['url']);
 
-$router->get('/posts', function () {
-    echo 'Tous les posts';
+$router = new App\Router\Router($_GET['url']);
+$router->get('/', function(){ echo 'je suis à l\'index';});
+$router->get('/:id', function($id){ echo 'je suis à la page'.$id;});
+$router->get('/posts', function(){ echo 'Tous les posts';});
+$router->get('/posts/:id', function($id){ 
+?>
+<form action="" method="post">
+    <input type="text" name="name" id="">
+    <button type="submit">Envoyer</button>
+</form>
+<?php
 });
-$router->get('/posts/:id', function ($id) {
-    echo "afficher le post n°" . $id;
-});
-$router->post('/posts/:id', function ($id) {
-    echo "poster le post n°" . $id;
-});
+$router->post('/posts/:id', function($id){ echo "poster le post n°".$id;});
+
 $router->drive_check();
+
+
+
+
+
+
